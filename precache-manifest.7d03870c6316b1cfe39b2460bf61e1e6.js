8da98a0b42fb0b6b12938535dd42846df0f3b03a
Ana Paula <anapcwb@gmail.com>
2023-04-10 17:56:17 +0100

    Updates

---
+++ precache-manifest.7d03870c6316b1cfe39b2460bf61e1e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc53b5582ecde42783014df961ad96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2389a8515f1357ead6ba",
+    "url": "/static/css/main.9749fbc3.chunk.css"
+  },
+  {
+    "revision": "1839c9d32e6633fb2a9a",
+    "url": "/static/js/2.426a6008.chunk.js"
+  },
+  {
+    "revision": "2389a8515f1357ead6ba",
+    "url": "/static/js/main.cccdb663.chunk.js"
+  },
+  {
+    "revision": "490200676ff39d15335e",
+    "url": "/static/js/runtime-main.5acd3983.js"
+  },
+  {
+    "revision": "3193678aaa3cdf85368e6bda511de7ba",
+    "url": "/static/media/Icon-material-cancel.3193678a.svg"
+  },
+  {
+    "revision": "4a08fb6590302033865752409f2e31b1",
+    "url": "/static/media/headerLogo.4a08fb65.svg"
+  },
+  {
+    "revision": "f2a5bd1247d4d69e7a63e41974f48919",
+    "url": "/static/media/loadingLogo.f2a5bd12.svg"
+  }
+]);
